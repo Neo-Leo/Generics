c5945b6623813e568f1307ec48d57e04f763dca9
Neo-Leo <sharm398@umn.edu>
2019-03-17 18:41:04 -0700

    Create SimpleGenerics.java

---
+++ SimpleGenerics.java
@@ -0,0 +1,28 @@
+class Custom<T> {
+
+    T t;
+
+    Custom(T t) {
+        this.t = t;
+    }
+
+    T get() {
+        return t;
+    }
+}
+
+public class SimpleGenerics {
+    public static void main(String[] args) {
+        Custom<Integer> intC = new Custom<Integer>(Integer.MAX_VALUE);
+        System.out.println(intC.get());
+
+        Custom<Long> longC = new Custom<Long>(Long.MAX_VALUE);
+        System.out.println(longC.get());
+
+        Custom<Double> doubleC = new Custom<Double>(Double.MAX_VALUE);
+        System.out.println(doubleC.get());
+
+        Custom<String> stringC = new Custom<String>("Test");
+        System.out.println(stringC.get());
+    }
+}
